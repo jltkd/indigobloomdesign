59f3a6df526bb1c60db29b88860f3b08f538db42
joelohr <joe@joelohrdesign.com>
2016-08-29 15:16:07 -0500

    Look and feel updates

--- js/custom.js
+++ js/custom.js
@@ -5,15 +5,16 @@ jQuery(document).ready(function(){
 		jQuery('.hovercard').not(this).stop().animate({
 			opacity: 0.3
 		}, 200);
-		jQuery('.hovercard p').not(this).stop().addClass('purple');
+		jQuery('.hovercard a').not(this).stop().addClass('purple');
 	});
 
 	jQuery('.hovercard').bind('mouseleave', function(){
 		jQuery('.hovercard').not(this).stop().animate({
 			opacity: 1
 		}, 200);
-		jQuery('.hovercard p').not(this).stop().removeClass('purple');
+		jQuery('.hovercard a').not(this).stop().removeClass('purple');
 	});
 
+	jQuery('p:empty').css('display', 'none');
 
 });
